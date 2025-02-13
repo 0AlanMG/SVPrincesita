08e3620429143f15abdb61b5b5c3b1932cda7a10
Alan Montes <esc.amdog5@gmail.com>
2025-02-13 02:45:04 -0600

    Funcionalidad Imagen Secreta

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,7 @@
 const txtPrincipal = document.getElementsByClassName("txt-principal")[0];
 const btnContinue = document.getElementsByClassName("btn-continue")[0];
 const imgSticker = document.getElementsByClassName("img-sticker")[0];
+const imgSecret = document.getElementsByClassName("img-secret")[0];
 
 //Inicializacion
 var countNumLine = 1;
@@ -89,6 +90,47 @@ const urlSticker = (numLine) => {
     }
 }
 
+const urlImgSecret = (numLine) => {
+    switch (numLine) {
+        case 1:
+            return "./assets/img/Secret Photos/Foto.jpg";
+        case 2:
+            return "./assets/img/Secret Photos/Foto1.jpg"
+        case 3:
+            return "./assets/img/Secret Photos/Foto2.jpg"
+        case 4:
+            return "./assets/img/Secret Photos/Mensaje.jpg"
+        case 5:
+            return "./assets/img/Secret Photos/Detalle2.jpg"
+        case 6:
+            return "./assets/img/Secret Photos/Detalle3.jpg"
+        case 7:
+            return "./assets/img/Secret Photos/Cancion1.jpg"
+        case 8:
+            return "./assets/img/Secret Photos/Detalle6.jpg"
+        case 9:
+            return "./assets/img/Secret Photos/Cancion.jpg"
+        case 10:
+            return "./assets/img/Secret Photos/Detalle4.jpg"
+        case 11:
+            return "./assets/img/Secret Photos/Cancion2.jpg"
+        case 12:
+            return "./assets/img/Secret Photos/Detalle1.jpg"
+        case 13:
+            return "./assets/img/Secret Photos/Detalle7.jpg"
+        case 14:
+            return "./assets/img/Secret Photos/Cancion5.jpg"
+        case 15:
+            return "./assets/img/Secret Photos/Cancion8.jpg"
+        case 16:
+            return "./assets/img/Secret Photos/Foto4.jpg"
+        case 17:
+            return "./assets/img/Secret Photos/Foto3.jpg"
+        default:
+            break;
+    }
+}
+
 //Functions (Changes)
 const writeTextPrincipal = (numLine) => {
     txtPrincipal.innerText = textPrincipalComplete(numLine);
@@ -98,17 +140,33 @@ const changeSticker = (numLine) => {
     imgSticker.src = urlSticker(numLine);
 }
 
+const changeImgSecret = (numLine) => {
+    imgSecret.src = urlImgSecret(numLine);
+}
+
+const changeDisplayImgSecret = (display) => {
+    imgSecret.style.display = display;
+}
+
 //Inicializacion HTML
 writeTextPrincipal(countNumLine);
 changeSticker(countNumLine);
 btnContinue.innerText = "Continuar...";
+changeImgSecret(countNumLine);
+changeDisplayImgSecret("none");
 
 //Clicks Events
 btnContinue.addEventListener("click", () => {
     countNumLine++;
     writeTextPrincipal(countNumLine);
     changeSticker(countNumLine);
+    changeImgSecret(countNumLine);
+    changeDisplayImgSecret("none");
     
     if (countNumLine > 15)
         btnContinue.style.display = "none";
+});
+
+imgSticker.addEventListener("click", () => {
+    changeDisplayImgSecret("block");
 });
